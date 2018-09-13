e6756ac7e2284272929c945a18d722533c2f1fca
Marcus Turnbo <marcus@emarcus.com>
2018-09-13 18:18:23 -0500

    Configured webpack and babel

---
+++ src/containers/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app-container">
+        <h1>React Redux Notification System</h1>
+        <hr />
+      </div>
+    );
+  }
+}
+
+export default App;
